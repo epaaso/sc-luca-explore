f0e3b8f4117f0f699a0842e3d1ac7b0d4de9ce68
Ernesto Paas <ernesto.paas@ciencias.unam.mx>
2025-01-14 06:00:07 +0000

    clusters for early and late
    missing name annotation, but abundances look fine.

--- nb_subCluster/faiss_cluster.py
+++ nb_subCluster/faiss_cluster.py
@@ -8,7 +8,7 @@ app = modal.App("leiden-clustering-faiss-gpu")
 # Define the container image with necessary dependencies
 image = (
     modal.Image.from_registry("netopaas/faiss:cugraph-24-12") # This one contains cuda 12.1.1 which is compatible with faiss-gpu 1.8.0
-    # .run_commands(
+    # .run_commands( # wE LEAVE THIS HERE TO SHOW HOW TO INSTALL FAISS-GPU
     #     [
     #         # Install Miniconda
     #         "wget https://repo.anaconda.com/miniconda/Miniconda3-py311_24.9.2-0-Linux-x86_64.sh -O /tmp/miniconda.sh",
@@ -37,7 +37,11 @@ image = (
     .env({"PATH": "/opt/conda/bin:$PATH"})
     .run_commands(
     [
-        "/opt/conda/bin/conda install -y modal"
+        # Strange behaviour because of using conda and pip, but there is no modal installer for conda
+        'pip install --upgrade google-api-python-client',
+        'pip install modal-client',
+        '/opt/conda/bin/conda install -y grpclib'
+
     ]
     )
 )
@@ -53,30 +57,24 @@ vol = modal.Volume.from_name("faiss-vol", create_if_missing=True)
     cpu=10,
     volumes={"/data": vol}
 )
-def leiden_clustering(nlist=1000, n_neighbors=30, resol=0.1, nprobe= 50, file_path='/data/query_latent_tumor.h5ad'):
+def leiden_clustering(nlist=1000, n_neighbors=30, resol=0.1, nprobe= 50,
+                    umap_params = None,
+                    file_path='/data/query_latent_tumor.h5ad'):
     import anndata as ad
     # import scanpy as sc
     import numpy as np
     import cupy as cp
+    from cupyx.scipy.sparse import coo_matrix
     import faiss
     import cudf
     import cugraph
-    import sys
     from cuml.manifold import UMAP
-    # import matplotlib as plt
 
     print('cugraph ver:')
     print(cugraph.__version__)
     from scipy.sparse import csr_matrix
     # Load your AnnData object
     adata = ad.read_h5ad(file_path)  # Ensure this path is correct
-
-    # Subset to only epithelial
-    # print(adata.shape)
-    # epit_types = ['Alveolar cell type 1', 'Alveolar cell type 2',  'ROS1+ healthy epithelial', 'transitional club/AT2', 'Club', 'Ciliated']
-    # epit_tumor_filter = np.logical_or(adata.obs.cell_type.isin(epit_types), adata.obs.cell_type.str.contains('Tumor'))
-    # adata = adata[epit_tumor_filter]
-    # print(adata.shape)
     
     # Ensure data is in numpy array format
     if not isinstance(adata.X, np.ndarray):
@@ -108,6 +106,7 @@ def leiden_clustering(nlist=1000, n_neighbors=30, resol=0.1, nprobe= 50, file_pa
     indices_df = cudf.DataFrame(indices[:, 1:])  # Exclude self-match at index 0
     distances_df = cudf.DataFrame(distances[:, 1:])
 
+
     # Create source and destination node arrays
     src = cudf.Series(
         np.repeat(np.arange(data.shape[0]), n_neighbors)
@@ -119,62 +118,65 @@ def leiden_clustering(nlist=1000, n_neighbors=30, resol=0.1, nprobe= 50, file_pa
 
     # Create a cuGraph Graph
     G = cugraph.Graph()
-    G.from_cudf_edgelist(
-        cudf.DataFrame({'src': src, 'dst': dst,
+
+    knn_cusparse = cudf.DataFrame({'src': src, 'dst': dst,
                         'weights': weights
-                       }),
+                       })
+    
+    G.from_cudf_edgelist(
+        knn_cusparse,
         source='src',
         destination='dst',
         edge_attr='weights',
         renumber=False
     )
 
-    # Compute layout on the GPU
-    
-    # pos_df = cugraph.force_atlas2(G)
-    
-    # # Visualize with matplotlib
-    # plt.scatter(pos_df['x'], pos_df['y'], s=10, alpha=0.7)
-    # plt.title("ForceAtlas2 Layout")
-    # plt.savefig('/data/foo.png')
 
-    # Convert your numpy data to a cuDF DataFrame or a cupy array
+    ######## UMAP PART ###########
     data_cudf = cudf.DataFrame(data)
 
+    if umap_params is None:
+        umap_params = {
+        "n_neighbors": 20,
+        "n_components": 2,
+        "min_dist": 0.1,
+        "spread": 1.0,
+        "random_state": 42,
+        "method": "cuml-umap",
+        }
+    
     # Instantiate UMAP
     umap_model = UMAP(
-        n_neighbors = n_neighbors,
-        n_components = 2,
-        # min_dist     = 0.1,
-        random_state = 42
+        **umap_params
     )
+
+    num_samples = data.shape[0]
+    # rows = cp.repeat(cp.arange(num_samples), indices_df.shape[1])
+    rows = cp.asarray(src)
+    cols = cp.asarray(dst)
+    vals = cp.asarray(weights)
+    knn_sparse = coo_matrix((vals, (rows, cols)), shape=(num_samples, num_samples)).tocsr()
+    
 
     # Fit-transform using the precomputed kNN
     #   - 'knn_indices' and 'knn_dists' must be passed here,
     #     so that UMAP won't recompute neighbors.
     umap_emb = umap_model.fit_transform(
         X=data_cudf,
-        knn_indices=dst,
-        knn_dists=weights
+        knn_graph=knn_sparse,
     )
 
     # 1. Convert the embedding to a NumPy array so we can store it in adata:
-    umap_emb_host = cp.asnumpy(umap_emb)
+    umap_emb_host = cudf.DataFrame.to_numpy(umap_emb)
 
     # 2. Insert the embedding into adata.obsm:
     adata.obsm["X_umap"] = umap_emb_host
 
     # Optionally, you can also store the UMAP parameters in adata.uns for reference:
-    umap_params = {
-        "n_neighbors": n_neighbors,
-        "n_components": 2,
-        "min_dist": 0.1,
-        "random_state": 42,
-        "method": "cuml-umap",
-    }
     adata.uns["umap_params"] = umap_params
 
-    # Perform Leiden clustering
+
+    ############# Perform LEIDEN clustering #######################################
     partitions_df, modularity = cugraph.leiden(G, resolution=resol, max_iter=1000)
     print("Modularity: ", modularity)
 
@@ -184,20 +186,40 @@ def leiden_clustering(nlist=1000, n_neighbors=30, resol=0.1, nprobe= 50, file_pa
 
     print('NUMCLUSTERS:')
     print(len(adata.obs.leiden.unique()))
+
+    print('UPARAMS')
+    print(umap_params)
+
+
     return adata.obs, umap_emb_host, umap_params
 
 @app.local_entrypoint()
 def main():
     import json
+    import pandas as pd    
 
     # file_path = '/data/query_latent_tumor_early.h5ad'
     file_path = '/data/query_latent_tumor_late.h5ad'
     suffix = file_path.split('/')[-1].split('.')[0].split('_')[-1]
+
+    umap_params = {
+        "n_neighbors": 30,
+        "n_components": 2,
+        "min_dist": 0.03,
+        "spread": 0.6,
+        "random_state": 42,
+        "method": "cuml-umap",
+    }
+
+    resol = 0.5
+
     # n_probe <= nlist means more accurate neighbours
-    leiden_df, umap_emb, umap_params = leiden_clustering.remote(nlist=1, n_neighbors=20, resol=0.5, nprobe=1, file_path=file_path)
+    leiden_df, umap_emb, umap_params = leiden_clustering.remote(nlist=1, resol=resol, nprobe=1,
+                                                                umap_params=umap_params, file_path=file_path)
+    umap_params['leiden_resol'] = resol
 
     leiden_df.to_csv(f'{subluster_path}/atlas_{suffix}_leiden.csv')
-    umap_emb.to_csv(f'{subluster_path}/atlas_{suffix}_umap.csv')
+    pd.DataFrame(umap_emb).to_csv(f'{subluster_path}/atlas_{suffix}_umap.csv')
     
-    with open(f'{subluster_path}/atlas_{suffix}_uparams', 'w') as fp:
+    with open(f'{subluster_path}/atlas_{suffix}_uparams.json', 'w') as fp:
         json.dump(umap_params, fp)
